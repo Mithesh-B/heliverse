0f3d6e951afc4e5d26bcefb8661d9170fdc68215
Mithesh B <mitheshbangera70@gmail.com>
2023-04-14 20:08:12 +0530

    export to cart

---
+++ src/components/dashboard/Team.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './team.scss'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeItem } from '../cards/cartSlice'
+
+const Team = () => {
+    const products = useSelector(state=>state.cart.products)
+    const dispach =useDispatch()
+
+
+  return (
+    <div className='team'>
+      <div className="teamContent">
+        My team
+        {products.map((item)=>(
+            <div key={item.id} className='content'>
+                <div className="left">
+                    <div className='name'>{item.firstName} {item.lastName}</div>
+                    <div className='email'>{item.email}</div>
+                    <div className='domain'>{item.domain}</div>
+                </div>
+                <div className="right">
+                    <img src={item.image} alt="" />
+                    <button onClick={()=>dispach(removeItem(item.id))}>remove</button>
+                </div>
+            </div>
+        ))}
+       
+      </div>
+    </div>
+  )
+}
+
+export default Team
