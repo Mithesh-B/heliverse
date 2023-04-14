e09b7825897edd356b0f813d12d1cf66d8038bb5
Mithesh B <mitheshbangera70@gmail.com>
2023-04-14 12:14:11 +0530

    add filter items+pass props

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,67 @@
 import React from 'react'
+
 import './dashboard.scss'
 
-const Dashboard = () => {
+const Dashboard = ({onChange, gender, available, domain}) => {
+
+
+
+
   return (
     <div className='dashboard'>
       <div className="top"><h1>Team Up</h1></div>
       <div className="bottom">
         <div className="left">
-            <div className="search"><input type="text" /></div>
+            <div className="search"><input type="text" onChange={onChange} /></div>
         </div>
         <div className="right">
-            <div className="filter">Filter</div>
+          <div>
+            <div className="filter">
+              <label htmlFor="gender-select">Gender:</label>
+                  <select
+                    id="gender-select"
+        
+                    onChange={gender}
+                  >
+                    <option value="">All</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+          </div>
+          <div>
+          <div className="available">
+              <label htmlFor="gender-select">Availability:</label>
+                  <select
+                    id="gender-select"
+        
+                    onChange={available}
+                  >
+                    <option value="">All</option>
+                    <option value="true">Available</option>
+                    <option value="false">Unavailable</option>
+                  </select>
+                </div>
+          </div>
+          <div>
+          <div className="domain">
+              <label htmlFor="gender-select">Domain:</label>
+                  <select
+                    id="gender-select"
+        
+                    onChange={domain}
+                  >
+                    <option value="">All</option>
+                    <option value="Sales">Sales</option>
+                    <option value="Finance">Finance</option>
+                    <option value="Marketing">Marketing</option>
+                    <option value="Management">Management</option>
+                    <option value="IT">IT</option>
+                    <option value="UI Designing">UI Designing</option>
+                    <option value="Business Development">Business Development</option>
+                  </select>
+                </div>
+          </div>
             <div className="team">My Team</div>
         </div>
       </div>
